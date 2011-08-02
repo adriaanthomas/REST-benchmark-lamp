d5f332d424e452c36992974a0ac964a6b9dcead6
Adriaan Thomas <athomas@xebia.com>
2011-08-02 19:09:06 +0200

    Removed the old MyISAM table.

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -6,17 +6,8 @@ use rest;
 create user 'rest'@'localhost' identified by 'rest';
 grant select,insert,update,delete on rest.* to 'rest'@'localhost';
 
-CREATE TABLE rest_data (
-  id MEDIUMINT UNSIGNED NOT NULL,
-  short_string VARCHAR(200) NOT NULL,
-  long_string VARCHAR(3000) NOT NULL,
-  int_number INT NOT NULL,
-  true_or_false BIT NOT NULL,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE rest_data2 (
-  id MEDIUMINT UNSIGNED NOT NULL,
+  id INT UNSIGNED NOT NULL,
   short_string VARCHAR(200) NOT NULL,
   long_string VARCHAR(3000) NOT NULL,
   int_number INT NOT NULL,
